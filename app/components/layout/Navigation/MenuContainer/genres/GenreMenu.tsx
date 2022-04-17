b594d5735f1dd4e2a1b3bc4f27ee843189881883
kuvanch <kolyabek13@gmail.com>
2022-04-17 18:14:43 +0500

    added meta and skeleton loader

--- app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,3 +1,5 @@
+import SkeletonLoader from '@/components/ui/SkeletonLoader'
+
 import Menu from '../Menu'
 
 import usePopularGenres from './usePopularGenres'
@@ -6,7 +8,9 @@ const GenreMenu = () => {
 	const { isLoading, data } = usePopularGenres()
 
 	return isLoading ? (
-		<div className="mx-11 mb-6">...Loading</div>
+		<div className="mx-11 mb-6">
+			<SkeletonLoader count={5} className="h-7 mt-6" />
+		</div>
 	) : (
 		<Menu menu={{ title: 'Popular genres', items: data || [] }} />
 	)
